2d347aa8bf1aaf6a558306460ccd547cb52853e7
Michael Scholz <michael@wellthapp.com>
2024-04-28 07:35:36 -0700

    simply test to just typescript types failing to load on serve preset

--- codegen.ts
+++ codegen.ts
@@ -2,21 +2,10 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 import { defineConfig } from "@eddeee888/gcg-typescript-resolver-files";
 
 const config: CodegenConfig = {
-  schema: ["./moduleFile.ts", "./schemaFile.graphql"],
+  schema: ["./moduleFile.ts"],
   require: ["ts-node/register"],
   generates: {
-    schema: {
-      ...defineConfig(),
-      config: {
-        enumValues: {
-          Color: {
-            RED: "red",
-            GREEN: "green",
-            BLUE: "blue",
-          },
-        },
-      },
-    },
+    schema: defineConfig(),
   },
 };
 
